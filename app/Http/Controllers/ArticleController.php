34a24c011ceb799896822933761f9b911e9477a3
harsenn24 <caesarharsenn@gmail.com>
2023-05-06 17:38:14 +0700

    finish crud article table

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $articles = Article::paginate(10);
+        return response()->json([
+            'data' => $articles
+        ]);
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $articles = Article::create([
+            'Title' => $request->title,
+            'Content' => $request->content,
+            'Articleimage' => $request->article_image,
+            'Articlecreator' => $request->article_creator,
+        ]);
+
+        return response()->json([
+            'data' => $articles
+        ]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Article $article)
+    {
+        return response()->json([
+            'data' => $article
+        ]);
+    }
+
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Article $article)
+    {
+        $article->Title = $request->title;
+        $article->Content = $request->content;
+        $article->Articleimage = $request->article_image;
+        $article->Articlecreator = $request->article_creator;
+        $article->save();
+
+        return response()->json([
+            'data' => $article
+        ]);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return response()->json([
+            'message' => 'article data deleted'
+        ], 204);
+    }
+}
